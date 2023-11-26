16bd67d6c258074cee127102db18933c2dad1867
Brayden Sue <bms8@sfu.ca>
2023-11-26 00:15:32 -0800

    calculated pearson correlation for two fields

--- src/run.py
+++ src/run.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from scipy import stats
 from amazing import functions as f
 
 currentDir = os.getcwd()
@@ -14,6 +15,17 @@ for hrRange in audioTable['heartrate'].values:
     bounds = hrRange.split("-")
     
     avgHeartRates.append((int(bounds[0]) + int(bounds[1]))/2)
-    
+
+
+# correlation between sleep duration & heart rate
+x = avgHeartRates
+y = audioTable['sleepDuration'].values
+corr, pVal = stats.pearsonr(x, y)
+plt.scatter(x, y)
+title = "corr = " + str(corr)
+plt.title(title)
+
+plt.show()
+
 
 # for i in range(len(audioTable['id'].values)):
